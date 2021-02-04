f1857a19bd92db2a5105a251c2c7c314fb44396b
Barry Mattern <bdmattern@gmail.com>
2021-02-03 22:15:42 -0700

    Add user, user-themes, user-favorites routes

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,13 +2,59 @@ const express = require('express');
 const { check } = require('express-validator');
 const asyncHandler = require('express-async-handler');
 
-const { User } = require('../../db/models');
+const { User, Theme, Favorite } = require('../../db/models');
 
 const router = express.Router();
 
-router.get('/', asyncHandler(async function (req, res, next) {
-    const users = await User.findAll();
-    res.json({ users });
+/*
+Route order matters, file reads top to bottom
+*/
+
+// // GET all users
+// router.get('/', asyncHandler(async function (req, res, next) {
+//     const users = await User.findAll();
+//     res.json({ users });
+// }));
+
+// GET one user by id
+router.get('/:userId', asyncHandler(async function (req, res, next) {
+  const user = await User.findByPk(`${req.params.userId}`);
+  res.json({ user });
+}));
+
+// GET one user's themes
+router.get('/:userId/themes', asyncHandler(async function (req, res, next) {
+  const userThemes = await Theme.findAll({
+    where: {
+      user_id: `${req.params.userId}`,
+    },
+  });
+  return res.json({ userThemes });
+}));
+
+// GET one user's favorite themes
+router.get(
+  '/:userId/themes/favorites',
+  asyncHandler(async function (req, res, next) {
+    const favoriteThemes = await Favorite.findAll({
+      where: {
+        user_id: `${req.params.userId}`,
+      },
+    });
+    return res.json({ favoriteThemes });
+  }),
+);
+
+// GET one user's single theme
+router.get(
+  '/:userId/themes/:themeId',
+  asyncHandler(async function (req, res, next) {
+    const userTheme = await Theme.findByPk(`${req.params.themeId}`, {
+      where: {
+        user_id: `${req.params.userId}`,
+      },
+    });
+    return res.json({ userTheme });
 }));
 
 module.exports = router;
