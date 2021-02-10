32f690afab93b3f470a456a977720c64f993b5a2
Barry Mattern <bdmattern@gmail.com>
2021-02-10 12:41:11 -0700

    * Restore package.json to previous version * Update routes in backend/routes/index.js

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -12,7 +12,7 @@ if (process.env.NODE_ENV === 'production') {
   router.get('/', (req, res) => {
     res.cookie('XSRF-TOKEN', req.csrfToken());
     return res.sendFile(
-      path.resolve(__dirname, '../frontend', 'build', 'index.html')
+      path.resolve(__dirname, '../../frontend', 'build', 'index.html')
     );
   });
 
@@ -23,7 +23,7 @@ if (process.env.NODE_ENV === 'production') {
   router.get(/^(?!\/?api).*/, (req, res) => {
     res.cookie('XSRF-TOKEN', req.csrfToken());
     return res.sendFile(
-      path.resolve(__dirname, '../frontend', 'build', 'index.html')
+      path.resolve(__dirname, '../../frontend', 'build', 'index.html')
     );
   });
 }
