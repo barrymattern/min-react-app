cb6adb6638e4f4ea6d2b7a95ce69eea9bf78c086
Barry Mattern <bdmattern@gmail.com>
2021-02-17 21:49:24 -0700

    Fix all errors, site renders

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,18 +4,13 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 import * as sessionActions from "./store/session";
 import Navigation from "./components/Navigation";
 import ProtectedRoute from "./components/ProtectedRoute";
-// import UserList from "./components/UsersList";
 import Signup from "./components/Signup";
 import Login from "./components/Login";
 import HomePage from "./components/HomePage";
 import Themes from "./components/Themes";
-import LightThemes from "./components/LightThemes";
-import DarkThemes from "./components/DarkThemes";
 import SingleTheme from "./components/SingleTheme";
 import UserProfile from "./components/UserProfile";
 import UserThemes from "./components/UserThemes";
-import UserLightThemes from "./components/UserLightThemes";
-import UserDarkThemes from "./components/UserDarkThemes";
 import UserSingleTheme from "./components/UserSingleTheme";
 import NotFound from "./components/NotFound";
 
@@ -59,14 +54,6 @@ function App() {
             <Themes />
           </Route>
 
-          <Route exact path="/themes/light">
-            <LightThemes />
-          </Route>
-
-          <Route exact path="/themes/dark">
-            <DarkThemes />
-          </Route>
-
           <Route exact path="/themes/:themeId">
             <SingleTheme />
           </Route>
@@ -77,14 +64,6 @@ function App() {
 
           <ProtectedRoute exact path="/user/themes" authenticated={authenticated}>
             <UserThemes />
-          </ProtectedRoute>
-
-          <ProtectedRoute exact path="/user/themes/light" authenticated={authenticated}>
-            <UserLightThemes />
-          </ProtectedRoute>
-
-          <ProtectedRoute exact path="/user/themes/dark" authenticated={authenticated}>
-            <UserDarkThemes />
           </ProtectedRoute>
 
           <ProtectedRoute exact path="/user/themes/:themeId" authenticated={authenticated}>
