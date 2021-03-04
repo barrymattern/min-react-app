504c6f7517690748d1470dad38a7e6b8527dfd80
Barry Mattern <bdmattern@gmail.com>
2021-03-03 22:25:47 -0700

    Add Footer CSS and text for links

--- frontend/src/components/Footer/index.js
+++ frontend/src/components/Footer/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import './Footer.css';
 
 const Footer = () => {
 
   return (
-    <h1>Footer!</h1>
+    <div className='footer__container'>
+      <ul className='social__links'>
+        <li className='link'>LinkedIn</li>
+        <li className='link'>AngelList</li>
+        <li className='link'>Github</li>
+        <li className='link'>Email</li>
+      </ul>
+    </div>
   );
 };
 
