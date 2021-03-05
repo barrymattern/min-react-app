ec66273849cdcc52f8b9e981d8e41ae03e747d90
Barry Mattern <bdmattern@gmail.com>
2021-03-04 21:19:15 -0700

    Add links to Footer, update CSS

--- frontend/src/components/Footer/index.js
+++ frontend/src/components/Footer/index.js
@@ -2,14 +2,35 @@ import React from 'react';
 import './Footer.css';
 
 const Footer = () => {
+  const links = {
+    linkedIn: 'https://www.linkedin.com/in/barrymattern/',
+    angelList: 'https://angel.co/u/barry-mattern',
+    gitHub: 'https://github.com/barrymattern',
+    email: 'mailto:bdmattern@gmail.com',
+  };
 
   return (
     <div className='footer__container'>
       <ul className='social__links'>
-        <li className='link'>LinkedIn</li>
-        <li className='link'>AngelList</li>
-        <li className='link'>Github</li>
-        <li className='link'>Email</li>
+        <li
+          className='link'
+          onClick={() => window.open(links.linkedIn, '_blank')}  
+        >LinkedIn</li>
+        <li className='separator'>|</li>
+        <li
+          className='link'
+          onClick={() => window.open(links.angelList, '_blank')}  
+        >AngelList</li>
+        <li className='separator'>|</li>
+        <li
+          className='link'
+          onClick={() => window.open(links.gitHub, '_blank')}  
+        >Github</li>
+        <li className='separator'>|</li>
+        <li
+          className='link'
+          onClick={() => window.open(links.email, '_blank')}  
+        >Email</li>
       </ul>
     </div>
   );
